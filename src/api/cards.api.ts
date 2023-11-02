a4a30e5af8ca0b937ef88e808964ebe5124f7ce9
Lucas Barros <lucasbarros.al@gmail.com>
2023-11-02 16:23:23 -0300

    feat: create env for develop

--- src/api/cards.api.ts
+++ src/api/cards.api.ts
@@ -2,9 +2,11 @@ import * as Crypto from 'expo-crypto'
 import { Card } from '@/entities'
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
+const baseUrl = process.env.EXPO_PUBLIC_API_URL
+
 export const cardsApi = createApi({
   reducerPath: 'cardsApi',
-  baseQuery: fetchBaseQuery({ baseUrl: 'http://10.0.2.2:3000' }),
+  baseQuery: fetchBaseQuery({ baseUrl: baseUrl || 'http://localhost:3000' }),
   tagTypes: ['Cards'],
   endpoints: builder => ({
     createCard: builder.mutation<Card, Omit<Card, 'id'>>({
